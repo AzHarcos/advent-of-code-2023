63367b1f93bbc7a7b59b5d90557f74a042082fcd
AzHarcos <luc718@hotmail.de>
2023-12-04 22:42:51 +0100

    day 0

--- src/day0.ts
+++ src/day0.ts
@@ -4,8 +4,10 @@ import {sum, mul, min, max, sortAsc, sortDsc, getChunks, sumReducer, mulReducer}
 const solveDay0 = () => {
   const input = readInputFile(2023, 0).split('\n');
 
+  console.log(input);
+
   // Part 1:
-  console.log(`Part 1: ${input}`);
+  //console.log(`Part 1: ${input}`);
 };
 
 solveDay0();
