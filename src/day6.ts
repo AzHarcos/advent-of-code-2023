d111b78bf67f5d7479140d48917f041963aa41a0
AzHarcos <luc718@hotmail.de>
2023-12-07 02:01:29 +0100

    day 6 wip

--- src/day6.ts
+++ src/day6.ts
@@ -34,26 +34,28 @@ const solveDay6 = () => {
   //console.log(`Part 1: ${input}`);
 };
 
-const solveDay62 = () => {
+const solveDay6_2 = () => {
   const input = readInputFile(2023, 6).split('\n');
 
   const time = Number(input[0].split(' ').slice(1).filter(Boolean).join(''));
 
   const distance = Number(input[1].split(' ').slice(1).filter(Boolean).join(''));
 
-  let waysOfRace = 0;
+  // T = total time, t = charge time, d = distance: f(t) = (T - t) * t = d
+  //                                             => f(t) = -t^2 + T*t - d = 0 => T und d gegeben also nach t lösen
+  //                                             => f(t) = t^2 - T*t + d = 0 => pq Formel, p = -T, q = d
+  //                                                     => T/2 +- root((-T/2)^2-d) => T / 2 +- root((T/2)^2-d)
 
-  for (let h = 1; h < time; h++) {
-    const travel = (time - h) * h;
-    if (travel > distance) {
-      waysOfRace++;
-    }
-  }
+  const maxTime = time / 2 + Math.sqrt((time / 2) ** 2 - distance);
 
-  console.log(waysOfRace);
+  const minTime = time / 2 - Math.sqrt((time / 2) ** 2 - distance);
+
+  const waysToWin = Math.floor(maxTime) - Math.ceil(minTime) + 1;
+
+  console.log(waysToWin);
 
   // Part 1:
   //console.log(`Part 1: ${input}`);
 };
 
-solveDay62();
+solveDay6_2();
